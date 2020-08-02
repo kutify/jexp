6849e4368508bd57483ea6995c14155d1bdf5c75
kutioff <stepan.kutyev@gmail.com>
2020-08-03 00:06:08 +0300

    #4 Allow non-integer exponents in power() method

--- src/test/java/io/github/kutify/math/expression/ExpressionTest.java
+++ src/test/java/io/github/kutify/math/expression/ExpressionTest.java
@@ -1,6 +1,10 @@
 package io.github.kutify.math.expression;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,6 +21,27 @@ class ExpressionTest {
                                 .with("b", 2)
                                 .build()
                 ).toString()
+        );
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("getPowTestArgs")
+    void powerOperationTest(String expression, String expected) {
+        String actual = String.valueOf(
+                Expression.compile(expression)
+                        .calculate(Arguments.EMPTY)
+                        .doubleValue()
+        );
+        assertEquals(
+                expected,
+                actual.substring(0, expected.length())
+        );
+    }
+
+    static Stream<org.junit.jupiter.params.provider.Arguments> getPowTestArgs() {
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of("(27)^(1/3)", "3.0"),
+                org.junit.jupiter.params.provider.Arguments.of("(2)^(1/2)", "1.414")
         );
     }
 }
